0b01d39c91581a75e98470ba3725d98c3efce737
zivpolack <polackziv@gmail.com>
2017-06-22 00:37:37 +0300

    Unit table

--- src/unit-table.ts
+++ src/unit-table.ts
@@ -1,5 +1,29 @@
-/**
- * Created by Ziv on 15/06/2017.
- */
 
-export const UnitTable = {};
+
+export const UNITS = {
+    meter: 'meter',
+    km: 'km',
+    cm: 'cm',
+    mile: 'mile',
+    yard: 'yard',
+    feet: 'feet',
+    inch: 'inch',
+    nauticalMile: 'nauticalMile'
+};
+
+
+export const UNIT_TABLE = {
+    distance: {
+        meter: {
+            baseUnit: UNITS.meter,
+            ratio: 1
+        },
+        km: {
+            baseUnit: UNITS.meter,
+            ratio: 1000
+        },
+        cm : {
+            baseUnit: UNITS.meter,
+            ratio: 0.01
+        }
+    }};
