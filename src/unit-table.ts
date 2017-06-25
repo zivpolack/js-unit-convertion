735f10e4ac949bdd744cdbfba143c3367fb56f34
Ziv Polack <polackziv@gmail.com>
2017-06-25 14:07:18 +0300

    Unit table update

--- src/unit-table.ts
+++ src/unit-table.ts
@@ -1,29 +1,88 @@
-
-
 export const UNITS = {
-    meter: 'meter',
-    km: 'km',
-    cm: 'cm',
-    mile: 'mile',
-    yard: 'yard',
-    feet: 'feet',
-    inch: 'inch',
-    nauticalMile: 'nauticalMile'
+    distance: {
+        meter: 'meter',
+        km: 'km',
+        cm: 'cm',
+        mile: 'mile',
+        yard: 'yard',
+        feet: 'feet',
+        inch: 'inch',
+        nauticalMile: 'nauticalMile'
+    },
+    weight: {
+        kg: 'kg',
+        lb: 'lb',
+        gram: 'gram',
+        miligram: 'miligram',
+        ton: 'ton'
+    },
+    volume: {
+        liter: 'liter',
+        gallon: 'gallon',
+        cubicMeter: 'cubicMeter',
+        cubicCm: 'cubicCm'
+    },
+    area: {
+        sqrMeter: 'sqrMeter',
+        sqrKm: 'sqrKm',
+        sqrNauticalMile: 'sqrNauticalMile',
+        sqrFeet: 'sqrFeet',
+        sqrCm: 'sqrCm'
+    },
+    temperature: {
+        celsius: 'celsius',
+        fahrenheit: 'fahrenheit',
+        kelvin: 'kelvin'
+    },
+    speed: {
+        meterPerSecond: 'meterPerSecond',
+        knot: 'knot',
+        mph: 'mph',
+        kph: 'kph',
+        mach: 'mach'
+    },
+    frequency: {
+        kHz: 'kHz',
+        hz: 'Hz',
+        mHz: 'mHz',
+        gHz: 'gHz'
+    },
+    pressure: {
+        inchesOfMercury: 'inchesOfMercury',
+        kgPerSqrCm: 'kgPerSqrCm',
+        lbPerSqrInch: 'lbPerSqrInch',
+        mmOfMercury: 'mmOfMercury',
+        bar: 'bar',
+        milibar: 'milibar'
+    },
+    angle: {
+        deg: 'deg',
+        rad: 'rad',
+        grad: 'grad',
+        minute: 'minute',
+        second: 'second'
+    },
+    jetFuelAmount: {
+        lb: 'lb',
+        gallon: 'gallon',
+        liter: 'liter',
+        kg: 'kg',
+        ton: 'ton'
+    },
 };
 
 
 export const UNIT_TABLE = {
     distance: {
+        baseUnit: UNITS.distance.meter,
         meter: {
-            baseUnit: UNITS.meter,
             ratio: 1
         },
         km: {
-            baseUnit: UNITS.meter,
             ratio: 1000
         },
-        cm : {
-            baseUnit: UNITS.meter,
+        cm: {
             ratio: 0.01
         }
-    }};
+    }
+};
